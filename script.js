1be41cb99d528cf2c08be738002deb3314857989
TheRookiexe <mishrxa36@gmail.com>
2025-09-06 21:49:33 +0530

    Grids are working. Add js logic for grids and css.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('.container');
 
-const grid = document.createElement('div');
-grid.classList.add('sqs');
-grid.setAttribute('style', "border: 2px black soild; background: white");
-container.append(grid);
+function grids(n,k){
+    for(let i=0; i<n; i++){
+        const row = document.createElement('div')
+        row.classList.add('row')
+        container.append(row)
+        for(let j=0; j<k; j++){ 
+            const grid = document.createElement('div');
+            grid.classList.add('sqs');
+            row.append(grid);
+        }
+    }
+}
+
+grids(16,16);
