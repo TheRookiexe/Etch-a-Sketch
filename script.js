06daab7040cba495cc0846808536def449c5222c
TheRookiexe <mishrxa36@gmail.com>
2025-09-06 15:39:39 +0530

    Cotainer div created and working

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+
+const grid = document.createElement('div');
+grid.classList.add('sqs');
+grid.setAttribute('style', "border: 2px black soild; background: white");
+container.append(grid);
