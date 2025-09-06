588b3ed8e966b43e1a881958f92055c72aef1efd
TheRookiexe <mishrxa36@gmail.com>
2025-09-06 23:10:21 +0530

    Draw mechanism working.
    Added js logic for drawing in the container.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const container = document.querySelector('.container');
 
 function grids(n,k){
     for(let i=0; i<n; i++){
-        const row = document.createElement('div')
-        row.classList.add('row')
-        container.append(row)
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.append(row);
         for(let j=0; j<k; j++){ 
             const grid = document.createElement('div');
             grid.classList.add('sqs');
@@ -14,3 +14,26 @@ function grids(n,k){
 }
 
 grids(16,16);
+
+let mousedown = false;
+
+document.addEventListener('mousedown', ()=>{
+    mousedown = true;
+});
+document.addEventListener('mouseup', ()=>{
+    mousedown = false;
+});
+
+const boxes = document.querySelectorAll('.sqs');
+
+boxes.forEach(box => {
+    box.addEventListener('mousedown', ()=>{
+        box.style.backgroundColor = 'yellow';
+    });
+
+    box.addEventListener('mouseenter', ()=>{
+        if (mousedown){
+            box.style.backgroundColor = 'yellow';
+        }
+    });
+});
